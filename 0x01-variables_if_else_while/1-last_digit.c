183bfafb7a4a90dad8487f30d61f27535cba1964
ElbeSongo <elbe.songo@gmail.com>
2024-01-27 03:04:46 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 
 	lastDigit = n % 10;
 
-	printf("lastDigit Of %d is %d and is ", n, lastDigit);
+	printf("last digit Of %d is %d and is ", n, lastDigit);
 
 	if (lastDigit > 5)
 	{
