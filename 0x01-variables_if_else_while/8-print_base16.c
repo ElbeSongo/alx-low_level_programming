a7a2456fa661c70e2be982926e823e91007481fe
ElbeSongo <elbe.songo@gmail.com>
2024-01-27 04:29:20 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints single-digit hexadecimal numbers in lowercase followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex_digit;
+
+	for (hex_digit = '0'; hex_digit <= '9'; ++hex_digit)
+		putchar(hex_digit);
+
+	for (hex_digit = 'a'; hex_digit <= 'f'; ++hex_digit)
+		putchar(hex_digit);
+
+	putchar('\n');
+
+	return (0);
+}
