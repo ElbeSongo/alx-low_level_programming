d2d03084ef753e468b66e90ebda7596eccc163ec
ElbeSongo <elbe.songo@gmail.com>
2024-01-27 03:48:59 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point, prints all single digit numbers of base 10 followed by a new line.
+ * main - Entry point, prints all single-digit numbers of base 10 followed by a new line.
  *
  * Return: Always 0 (Success)
  */
@@ -10,9 +10,9 @@ int main(void)
 	int digit;
 
 	for (digit = 0; digit < 10; ++digit)
-	{
+	
 		putchar(digit + '0');
-	}
+	
 
 	putchar('\n');
 
