914b387d732ad8773bb81f5fe5502b215600bd67
ElbeSongo <elbe.songo@gmail.com>
2024-01-31 01:26:17 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+
+/**
+ * _isalpha - checks if a character is an alphabetic letter.
+ * @c: the character to be checked.
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase; 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
