409e4f43936c160d551f54a44df520b75c75fbd9
ElbeSongo <elbe.songo@gmail.com>
2024-01-31 03:20:55 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,26 +7,18 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	while (n != 98)
 	{
-		for (int i = n; i <= 98; i++)
+		if (n < 98)
 		{
-			printf("%d", i);
-
-			if (i != 98)
-				printf(", ");
+			printf("%d, ", n);
+			n++;
+		}
+		else if (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
 		}
 	}
-	else
-	{
-		for (int i + n; i >= 98; i--)
-		{
-			printf("%d", i);
-
-			if (i != 98)
-				printf(", ");
-		}
-	}
-
-	printf("\n");
+	printf("%d\n", n);
 }
