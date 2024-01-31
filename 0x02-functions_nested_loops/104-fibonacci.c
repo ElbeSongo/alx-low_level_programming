397da907e5d5e8048b0002c1d91f0367387aa948
ElbeSongo <elbe.songo@gmail.com>
2024-01-31 05:00:48 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,8 +13,13 @@ int main(void)
 	int x = 3;
 
 	printf("%0.Lf, %0.Lf, ", val1, val2);
+
 	while (x <= 98)
 	{
+		fib = val1 + val2;
+		val1 = val2;
+		val2 = fib;
+
 		printf("%0.Lf", val2);
 
 		if (x != 98)
