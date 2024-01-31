237567dfe3e96023cbb512d1944b9aff16fa9d67
ElbeSongo <elbe.songo@gmail.com>
2024-01-31 02:54:18 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - adds two integers and returns the result.
- * @a; The first integer.
- * @b: The second integer.
+ * @a; The first integer to be added.
+ * @b: The second integer to be added.
  *
  * Return: The sum of a and b.
  */
