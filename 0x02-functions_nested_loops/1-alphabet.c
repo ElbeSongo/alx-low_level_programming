898717bf0da5b339a89a9d06ab59ba0d67eb46a3
ElbeSongo <elbe.songo@gmail.com>
2024-01-31 00:52:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this prints a-z.
+ */
+void print_alphabet(void)
+{
+	int alpha = 97;
+
+	while (alpha <= 122)
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
